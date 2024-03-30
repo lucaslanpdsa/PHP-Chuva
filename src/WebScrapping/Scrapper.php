8226e805b135db6caede3517f5560c9422ce17d1
lucas alves <lucasnascimento094@hotmail.com>
2024-03-30 10:24:29 -0300

    criando lista de authors

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -22,9 +22,6 @@ class Scrapper {
    */
   public function scrap(\DOMDocument $dom): array {
 
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
     function getElement($dom, $class){
       $elementosComClasse = $dom->getElementsByTagName('*');
       $elementos = [];
@@ -42,56 +39,34 @@ class Scrapper {
     $title = getElement($dom,'my-xs paper-title');
     $type = getElement($dom, 'tags mr-sm');
     $id = getElement($dom, 'volume-info');
-    var_dump($id);
 
-    function getauthors($dom){
+    function getAuthors($dom){
       $divAuthors = $dom->getElementsByTagName('div');
-      $authors = [];
+      $allAuthors = [];
   
       foreach ($divAuthors as $Author) {
         if ($Author->hasAttribute('class') && $Author->getAttribute('class') === 'authors') {
             $spans = $Author->getElementsByTagName('span');
+            $authorsOfPapper = [];
+
             foreach ($spans as $span) {
                 // Extrai o nome do autor
                 $name = $span->textContent;
                 // Extrai a instituição do autor do atributo title
                 $institution = $span->getAttribute('title');
                 // Adiciona o autor à lista de autores
-                $authors[] = new Person($name, $institution);
+                $authors = new Person($name, $institution);
+                $authorsOfPapper[] = $authors;
             }
+          $allAuthors[] = $authorsOfPapper;
         }
       }
   
-      var_dump($authors);
-      return $authors;
+      var_dump($allAuthors);
+      return $allAuthors;
     }
   
-    $authors = getauthors($dom);
+    $authors = getAuthors($dom);
     var_dump($authors);
-
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
     }
   }
-  
-  
-$scrapper = new Scrapper();
-
-// Crie um objeto DOMDocument e carregue o arquivo HTML
-$dom = new \DOMDocument('1.0', 'utf-8');
-$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
-// Chame o método scrap() passando o objeto DOMDocument como argumento
-$scrapData = $scrapper->scrap($dom);
-
-// Exiba o resultado
-var_dump($scrapData);
